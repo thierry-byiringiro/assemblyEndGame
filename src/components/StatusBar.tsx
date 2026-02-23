1b967265600ad2774961878e2bb72089bea3d700
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-23 12:58:18 +0200

    refactoring using prettier extension

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,10 +1,10 @@
-export default function StatusBar(){
-    return (
-        <>
-            <button className="w-90 h-18 p-6 bg-[#10A95B] flex flex-col items-center justify-center text-white rounded-md">
-                <h1 className="font-bold">You win!</h1>
-                <span className="font-medium">Well done! 🎉</span>
-            </button>        
-        </>
-    );
+export default function StatusBar() {
+  return (
+    <>
+      <button className="w-90 h-18 p-6 bg-[#10A95B] flex flex-col items-center justify-center text-white rounded-md">
+        <h1 className="font-bold">You win!</h1>
+        <span className="font-medium">Well done! 🎉</span>
+      </button>
+    </>
+  );
 }
