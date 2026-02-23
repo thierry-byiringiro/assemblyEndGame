e0346141f29469a7af978acf777d07e18607855d
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-23 10:46:50 +0200

    the header of the project

---
+++ src/components/Header.tsx
@@ -0,0 +1,13 @@
+export default function Header() {
+  return (
+    <>
+      <main className="flex flex-col items-center">
+        <h1 className="text-[#F9F4DA]  font-medium">Assembly: Endgame</h1>
+        <p className="text-[#8E8E8E] font-medium text-center w-90">
+          Guess the word in under 8 attempts to keep the programming world safe
+          from Assembly!
+        </p>
+      </main>
+    </>
+  );
+}
