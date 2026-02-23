9ffc8bb79223707c41d30a6b824367c94f204535
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-23 12:34:43 +0200

    displaying the words typed

---
+++ src/components/Words.tsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+export default function Words(){
+    const [word,setWord] = useState("react");
+    const getWords = word.split("").map((el,index) => <span key={index} className="h-9 w-10 border-b border-b-white border border-[#323232] bg-[#323232] flex justify-center items-center text-[#F9F4DA]  font-bold">{el.toUpperCase()}</span>)
+        return (
+            <>
+                <div className="flex gap-0.5">
+                    {getWords}
+                </div>
+            </>
+        );
+}
