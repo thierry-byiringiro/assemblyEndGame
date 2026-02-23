e64aad975635b844aa383d41dffc434ac217a1f0
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-23 12:58:38 +0200

    imported the keyboard component

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,20 @@
 import "./App.css";
 import Header from "./components/Header";
+import Keyboard from "./components/Keyboard";
+import Languages from "./components/Languages";
 import StatusBar from "./components/StatusBar";
+import Words from "./components/Words";
 
 function App() {
   return (
     <>
       <div className="flex justify-center items-center mt-5">
-        <div className="flex flex-col items-center w-138.5 h-151.5 bg-[#282726] py-12 px-10 space-y-5">
+        <div className="flex flex-col items-center w-138.5 h-151.5 bg-[#282726] py-12 px-8 space-y-6">
           <Header />
           <StatusBar/>
+          <Languages/>
+          <Words/>
+          <Keyboard/>
         </div>
       </div>
     </>
