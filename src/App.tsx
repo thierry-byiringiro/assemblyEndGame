fd41949e78742b96b7708246c9b65a7578c7ed05
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-23 10:47:33 +0200

    imported header and status bar component

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,18 @@
-import './App.css'
+import "./App.css";
+import Header from "./components/Header";
+import StatusBar from "./components/StatusBar";
 
 function App() {
-
   return (
     <>
-      <h1>AIME</h1>
+      <div className="flex justify-center items-center mt-5">
+        <div className="flex flex-col items-center w-138.5 h-151.5 bg-[#282726] py-12 px-10 space-y-5">
+          <Header />
+          <StatusBar/>
+        </div>
+      </div>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
