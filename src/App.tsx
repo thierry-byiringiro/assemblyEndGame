74293a71b534da354f8d7bbe770cb0e45c9ba76e
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-25 10:51:39 +0200

    removed the statusbar component

--- src/App.tsx
+++ src/App.tsx
@@ -9,7 +9,6 @@ function App() {
       <div className="flex justify-center items-center mt-5">
         <div className="flex flex-col items-center w-138.5 h-151.5 bg-[#282726] py-8 px-8 space-y-6">
           <Header />
-          <StatusBar/>
           <Keyboard/>
         </div>
       </div>
