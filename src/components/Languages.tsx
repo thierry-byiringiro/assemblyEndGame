9d5a19eb909a9248eabfcb72fb3511f8459be5f2
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-25 16:47:27 +0200

    returned the langueges component on its own

--- src/components/Languages.tsx
+++ src/components/Languages.tsx
@@ -1,14 +1,19 @@
 import Language from "../assets/languages.json";
-export default function Languages() {
-  const languages = Language.map((el, index) => (
-    <li
-      key={index}
-      style={{ backgroundColor: el.backgroundColor, color: el.color }}
-      className="p-1 flex justify-center font-bold rounded-sm"
-    >
-      {el.name}
-    </li>
-  ));
+import clsx from "clsx";
+export default function Languages({wrongGuessCount} : {wrongGuessCount : number}) {
+  const languages = Language.map((el, index) => {
+    const isLanguageLost = index < wrongGuessCount;
+    const classNames = clsx("chip", isLanguageLost && "lost");
+    return (
+      <li
+        key={index}
+        style={{ backgroundColor: el.backgroundColor, color: el.color }}
+        className={`${classNames} p-1 flex justify-center font-bold rounded-sm`}
+      >
+        {el.name}
+      </li>
+    );
+  });
   return (
     <>
       <div className="w-90.25 h-12.75">
