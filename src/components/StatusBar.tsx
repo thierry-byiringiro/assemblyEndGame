bd88c28759fe3ad573e16b592d97c4d787f25725
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-02-25 10:50:43 +0200

    added the values using props

--- src/components/StatusBar.tsx
+++ src/components/StatusBar.tsx
@@ -1,10 +1,33 @@
-export default function StatusBar() {
+export default function StatusBar({
+  isGameWon,
+  isGameLost,
+  languageNames,
+}: {
+  isGameWon: boolean;
+  isGameLost: boolean;
+  languageNames: string[];
+}) {
   return (
     <>
-      <button className="w-90 h-18 p-6 bg-[#10A95B] flex flex-col items-center justify-center text-white rounded-md">
-        <h1 className="font-bold">You win!</h1>
-        <span className="font-medium">Well done! 🎉</span>
-      </button>
+      {languageNames.length >= 1 && (
+        <button className="w-90 h-15 p-6 bg-[#7A5EA7] border border-dashed border-[#403354] flex flex-col items-center justify-center text-white rounded-sm">
+          <h1 className="font-bold">“Farewell {languageNames.join(" &   ")}🫡”</h1>
+        </button>
+      )}
+      {isGameWon && (
+        <button className="w-90 h-15 p-6 bg-[#10A95B] flex flex-col items-center justify-center text-white rounded-sm">
+          <h1 className="font-bold">You win!</h1>
+          <span className="font-medium">Well done! 🎉</span>
+        </button>
+      )}
+      {isGameLost && (
+        <button className="w-90 h-15 p-6 bg-[#BA2A2A] flex flex-col items-center justify-center text-white rounded-sm">
+          <h1 className="font-bold">Game Over!</h1>
+          <span className="font-medium">
+            You lose! Better start learning Assembly 😭
+          </span>
+        </button>
+      )}
     </>
   );
 }
