6aa291fc3e18c93c779e20550fa0fa7492df465f
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-03-31 23:58:55 +0200

    added the timer component

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,17 @@ import "./App.css";
 import Header from "./components/Header";
 import Keyboard from "./components/Keyboard";
 import StatusBar from "./components/StatusBar";
+import Timer from "./Timer/Timer";
 
 function App() {
   return (
     <>
-      <div className="flex justify-center items-center mt-5">
+      <div className="flex justify-center items-center mt-5 space-x-5">
         <div className="flex flex-col items-center w-138.5 h-151.5 bg-[#282726] py-8 px-8 space-y-6">
           <Header />
-          <Keyboard/>
+          <Keyboard />
         </div>
+        <Timer />
       </div>
     </>
   );
