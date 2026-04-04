bb3bcc4038e921a06e85b4af13c808b0585a585c
Aime Thierry Byiringiro <aimetiti8@gmail.com>
2026-04-04 22:23:33 +0200

    refactored the indentions using the prettier extension

--- src/components/Keyboard.tsx
+++ src/components/Keyboard.tsx
@@ -21,6 +21,7 @@ export default function Keyboard() {
     setCurrentWord(words[randomIndex]);
     setGuessedWord([]);
   };
+
   const wrongGuessesCount = guessedWord.filter((el) => {
     return !currentWord.includes(el);
   }).length;
@@ -33,15 +34,14 @@ export default function Keyboard() {
   const getWords = currentWord.split("").map((letter, index) => {
     const revealLetters = isGameLost || guessedWord.includes(letter);
     return (
-
-    <span
+      <span
       key={index}
       className="h-9 w-10 border-b border-b-white border border-[#323232] bg-[#323232] flex justify-center items-center text-[#F9F4DA]  font-bold"
-    >
-      {revealLetters ? letter.toUpperCase() : ""}
-    </span>
-  )
-});
+      >
+        {revealLetters ? letter.toUpperCase() : ""}
+      </span>
+    );
+  });
   const alphabets = "abcdefghijklmnopqrstuvwxyz";
   const getKeyboard = alphabets.split("").map((letter, index) => {
     const isGuessed = guessedWord.includes(letter);
@@ -53,13 +53,13 @@ export default function Keyboard() {
     });
     return (
       <button
-        style={{ backgroundColor: classNames }}
-        key={index}
-        value={letter}
-        onClick={() => addGuessedWords(letter)}
-        disabled={isGameOver}
-        aria-disabled={guessedWord.includes(letter)}
-        className={`${classNames} h-9 w-10 flex justify-center items-center text-black border border-white rounded-sm cursor-pointer bg-[#FCBA29]`}
+      style={{ backgroundColor: classNames }}
+      key={index}
+      value={letter}
+      onClick={() => addGuessedWords(letter)}
+      disabled={isGameOver}
+      aria-disabled={guessedWord.includes(letter)}
+      className={`${classNames} h-9 w-10 flex justify-center items-center text-black border border-white rounded-sm cursor-pointer bg-[#FCBA29]`}
       >
         {letter.toUpperCase()}
       </button>
@@ -67,18 +67,18 @@ export default function Keyboard() {
   });
   const LastGuessedLetter = guessedWord[guessedWord.length - 1];
   const isLastGussedIncorrect =
-    LastGuessedLetter && !currentWord.includes(LastGuessedLetter);
+  LastGuessedLetter && !currentWord.includes(LastGuessedLetter);
   const combine: boolean = !!(isLastGussedIncorrect && !isGameOver);
   return (
     <>
-    {isGameWon && <ReactConfetti/>}
+      {isGameWon && <ReactConfetti />}
       <StatusBar
         isGameWon={isGameWon}
         isGameLost={isGameLost}
         languageNames={languageNames}
         combine={combine}
         wrongGuesses={wrongGuessesCount - 1}
-      />
+        />
       <Languages wrongGuessCount={wrongGuessesCount} />
       <div className="flex gap-0.5 mt-5">{getWords}</div>
       <div className="flex flex-col space-y-4 items-center">
@@ -87,8 +87,8 @@ export default function Keyboard() {
         </div>
         {isGameOver && (
           <button
-            onClick={handleResetGame}
-            className="w-57 h-10 p-4 bg-[#11B5E5] flex items-center justify-center border border-white rounded-sm cursor-pointer"
+          onClick={handleResetGame}
+          className="w-57 h-10 p-4 bg-[#11B5E5] flex items-center justify-center border border-white rounded-sm cursor-pointer"
           >
             New Game
           </button>
